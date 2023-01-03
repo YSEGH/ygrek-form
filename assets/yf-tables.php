ec014e53d164e6b48f4e2181bd222d899809ecf2
youssef <youssef.segh@hotmail.fr>
2023-01-03 08:30:52 +0100

    Debut API REST

--- assets/yf-tables.php
+++ assets/yf-tables.php
@@ -27,25 +27,33 @@ if (!class_exists('YF_tables')) :
             global $wpdb;
             $plugin_name_db_version = '1.0';
             $charset_collate = $wpdb->get_charset_collate();
-            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-            $queries = [
-                "CREATE TABLE " . $wpdb->prefix . "_yf_form (
-                id mediumint(9) NOT NULL AUTO_INCREMENT,
-                form_id mediumtext NOT NULL,
-                form_class mediumtext NOT NULL,
-                form_theme tinytext NOT NULL,
-                rows longtext NOT NULL,
-                UNIQUE KEY id (id)
-                ) $charset_collate;",
-                "CREATE TABLE " . $wpdb->prefix . "_yf_submission (
-                id mediumint(9) NOT NULL AUTO_INCREMENT,
-                UNIQUE KEY id (id)
-                ) $charset_collate;",
-            ];
+            $form_table = $wpdb->prefix . 'yf_form';
+            $submission_table = $wpdb->prefix . 'yf_submission';
+            /* $queries = [
+                "CREATE TABLE $form_table (
+                    id mediumint(9) NOT NULL AUTO_INCREMENT,
+                    form_id mediumtext NOT NULL,
+                    form_class mediumtext NOT NULL,
+                    form_theme tinytext NOT NULL,
+                    rows longtext NOT NULL,
+                    UNIQUE KEY id (id)
+                    ) $charset_collate;",
+                "CREATE TABLE $submission_table (
+                    id mediumint(9) NOT NULL AUTO_INCREMENT,
+                    UNIQUE KEY id (id)
+                    ) $charset_collate;",
+            ]; */
+            $query = "CREATE TABLE $form_table (
+                    id mediumint(9) NOT NULL AUTO_INCREMENT,
+                    form_id mediumtext NOT NULL,
+                    form_class mediumtext NOT NULL,
+                    form_theme tinytext NOT NULL,
+                    rows longtext NOT NULL,
+                    UNIQUE KEY id (id)
+                    ) $charset_collate;";
 
-            foreach ($queries as $query) {
-                dbDelta($query);
-            }
+            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+            dbDelta($query);
 
             add_option('ygrek_form_db_version', $plugin_name_db_version);
         }
@@ -53,11 +61,13 @@ if (!class_exists('YF_tables')) :
         function remove_tables()
         {
             global $wpdb;
-            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+            $form_table = $wpdb->prefix . 'yf_form';
+            $submission_table = $wpdb->prefix . 'yf_submission';
             $queries = [
-                "DROP TABLE IF EXISTS " . $wpdb->prefix . "_yf_form",
-                "DROP TABLE IF EXISTS " . $wpdb->prefix . "_yf_submission",
+                "DROP TABLE IF EXISTS $form_table",
+                "DROP TABLE IF EXISTS $submission_table",
             ];
+            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
             foreach ($queries as $query) {
                 dbDelta($query);
             }
